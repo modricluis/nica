fe262a27bab1c306a3558fd825316813e2bcf341
modric <165011983+modricluis@users.noreply.github.com>
2024-05-28 16:59:34 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-8cfd0213-0e1e-4c14-a11f-9d4ff8f7fc78/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-7ffa5479-dc75-4c66-a517-c7e2d917735f/)
 
 
 echo "$curl_output" >> Update.tsx
